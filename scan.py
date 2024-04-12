8c20decd3bafe40d1060a4caef3111908ee1ed84
9jp <66310125+9jp@users.noreply.github.com>
2024-04-12 03:46:54 +0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,8 +1,5 @@
 import socket
-import art
-
-from art import
-   tprint("MJ")
+from ip2geotools.databases.noncommercial import DbIpCity
 
 print("CODED BY MJ, My Website: m-j.sa My Server: Discord.gg/mJm")
 
@@ -20,10 +17,18 @@ def get_registration_info(url):
     registration_info = socket.gethostbyaddr(socket.gethostbyname(url))[1]
     return registration_info
 
+def get_ip_location(ip):
+    response = DbIpCity.get(ip, api_key='free')
+    city = response.city
+    country = response.country
+    return city, country
+
 ip = get_ip(url)
 dns = get_dns(url)
 registration_info = get_registration_info(url)
+city, country = get_ip_location(ip)
 
 print("IP Address: ", ip)
 print("DNS Name: ", dns)
 print("Registration Info: ", registration_info)
+print("IP Location - City: {}, Country: {}".format(city, country))
